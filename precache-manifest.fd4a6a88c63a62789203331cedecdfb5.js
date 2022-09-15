94276f4375a5feb164d560331a453f13c0c28d41
BackendD <hasankarimi.dev@gmail.com>
2022-09-15 21:45:28 +0000

    Deploying to gh-pages from @ m-s-team/psychology-front@15df2e72465722ecf2e2928ca6ef631efdf393e8 🚀

---
+++ precache-manifest.fd4a6a88c63a62789203331cedecdfb5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f992eba17bc76b60624eff5d5df4e783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6454ad93b2bafbd93827",
+    "url": "/static/css/2.0e43e166.chunk.css"
+  },
+  {
+    "revision": "0d36b023ab60ac927936",
+    "url": "/static/css/main.e772358b.chunk.css"
+  },
+  {
+    "revision": "6454ad93b2bafbd93827",
+    "url": "/static/js/2.58109ff7.chunk.js"
+  },
+  {
+    "revision": "0d36b023ab60ac927936",
+    "url": "/static/js/main.b569d61e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/static/media/logo.a0185b04.svg"
+  }
+]);
